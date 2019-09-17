264dd4234e14975b2bc4be16271f086a8e17956d
Gina Graci <ginagraci92@gmail.com>
2019-09-17 20:03:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -21,3 +21,22 @@ end
 def shift_with_args(ice_cream_brands)
   ice_cream_brands.shift(2)
 end
+
+def using_concat
+  
+end
+
+def using_insert
+end
+
+def using_uniq
+end
+
+def using_flatten
+end
+
+def using_delete
+end
+
+def using_delete_at
+end
