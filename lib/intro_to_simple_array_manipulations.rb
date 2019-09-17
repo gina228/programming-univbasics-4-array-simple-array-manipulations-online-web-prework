7f62c0d33c9e1cf1a367365940964f42f1401417
Gina Graci <ginagraci92@gmail.com>
2019-09-17 20:33:23 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -30,7 +30,8 @@ def using_insert(list_of_programming_languages, another_language)
   list_of_programming_languages.insert(4, "Python")
 end
 
-def using_uniq()
+def using_uniq(haircuts)
+  haircuts.uniq
 end
 
 def using_flatten
